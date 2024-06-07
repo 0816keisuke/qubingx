ff4029f61d0bf55a9f34bae1c96f37087bdadebd
keisuke.fukada <keisukefukada.0816@gmail.com>
2024-06-08 07:07:23 +0900

    add: update setup.

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="qubingx",
     version="0.1.0",
-    install_requires=["numpy", "dimod", "plotly"],
+    install_requires=["numpy", "dimod", "dwave-system", "plotly"],
 )
